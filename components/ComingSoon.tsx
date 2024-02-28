aa346f7a8f37cfee2128e2131431abedccfc4ece
Mustafiz Kaifee Mumtaz <mustafiz.mumtaz@medibuddy.in>
2024-02-28 23:10:35 +0530

    first draft

---
+++ components/ComingSoon.tsx
@@ -0,0 +1,10 @@
+export default function ComingSoon () {
+  return (
+    <div className="flex items-center justify-center min-h-screen text-green-900">
+      <div className="text-center">
+        <h1 className="mb-4 text-5xl font-bold">Coming Soon</h1>
+        <p className="mb-4">I am working hard to finish the development of this site. Stay tuned!</p>
+      </div>
+    </div>
+  );
+};

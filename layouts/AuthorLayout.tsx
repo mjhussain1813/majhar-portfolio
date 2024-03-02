dddce1fe25b99c7c847943b12dc1a36ef9fd17d8
mjhussain1813 <mjhussain18131@gmail.com>
2024-03-02 14:35:16 +0530

    added codepal url

--- layouts/AuthorLayout.tsx
+++ layouts/AuthorLayout.tsx
@@ -33,7 +33,12 @@ export default function AuthorLayout({ children, content }: Props) {
             <h3 className="pb-2 pt-4 text-2xl font-bold leading-8 tracking-tight">{name}</h3>
             <div className="text-gray-500 dark:text-gray-400">{occupation}</div>
             <div className="text-gray-500 dark:text-gray-400">{company}</div>
-         <a href='https://www.codepal.in/'> <div className="text-gray-500 dark:text-blue-400 text-decoration:underline">Building Codepal</div></a>  
+            <a href="https://www.codepal.in/">
+              {' '}
+              <div className="text-decoration:underline text-gray-500 dark:text-blue-400">
+                Building Codepal
+              </div>
+            </a>
             {/* <div className="flex space-x-3 pt-6">
               <SocialIcon kind="mail" href={`mailto:${email}`} />
               <SocialIcon kind="github" href={github} />
